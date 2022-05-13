1733657a21cfaca32c6ebf063cb3bf2ce2304b90
kritoma <toma.kristian97@gmail.com>
2022-05-13 15:30:02 +0200

    js our team

--- js-our-team/js/main.js
+++ js-our-team/js/main.js
@@ -3,31 +3,49 @@ const container = document.querySelector(".team-container");
 const listCard = [{
     user : "Wayne Barnett",
     role : "Founder & CEO",
-    image : src = "img/wayne-barnett-founder-ceo.jpg",
+    image : "img/wayne-barnett-founder-ceo.jpg",
 },
 {
     user : "Angela Caroll",
     role : "Chief Editor",
-    image : src = "img/angela-caroll-chief-editor.jpg",
+    image : "img/angela-caroll-chief-editor.jpg",
 },
 {
     user : "Walter Gordon",
     role : "Office Manager",
-    image : src = "img/walter-gordon-office-manager.jpg",
+    image : "img/walter-gordon-office-manager.jpg",
 },
 {
     user : "Angela Lopez",
     role : "Social Media Manager",
-    image : src = "img/angela-lopez-social-media-manager.jpg",
+    image :"img/angela-lopez-social-media-manager.jpg",
 },
 {
     user : "Scott Estrada",
     role : "Developer",
-    image : src = "img/scott-estrada-developer.jpg",
+    image : "img/scott-estrada-developer.jpg",
 },
 {
     user : "Barbare Ramos",
     role : "Graphic Designer",
-    image : src = "img/barbara-ramos-graphic-designer.jpg",
+    image : "img/barbara-ramos-graphic-designer.jpg",
 },
 ];
+
+for (let i = 0; i < listCard.length; i++) {
+    const card =`
+        <div class="team-card">
+            <div class="card-image">
+              <img
+                src=${listCard[i].image}
+                alt=${listCard[i].user}
+              />
+            </div>
+            <div class="card-text">
+              <h3>${listCard[i].user}</h3>
+              <p>${listCard[i].role}</p>
+            </div>
+        </div>
+    `
+    container.innerHTML += card
+}
