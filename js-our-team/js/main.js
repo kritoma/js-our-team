2562ad6295fd9843de6b575409c6693872f4e54d
kritoma <toma.kristian97@gmail.com>
2022-05-13 15:22:41 +0200

    first commit

---
+++ js-our-team/js/main.js
@@ -0,0 +1,33 @@
+const container = document.querySelector(".team-container");
+
+const listCard = [{
+    user : "Wayne Barnett",
+    role : "Founder & CEO",
+    image : src = "img/wayne-barnett-founder-ceo.jpg",
+},
+{
+    user : "Angela Caroll",
+    role : "Chief Editor",
+    image : src = "img/angela-caroll-chief-editor.jpg",
+},
+{
+    user : "Walter Gordon",
+    role : "Office Manager",
+    image : src = "img/walter-gordon-office-manager.jpg",
+},
+{
+    user : "Angela Lopez",
+    role : "Social Media Manager",
+    image : src = "img/angela-lopez-social-media-manager.jpg",
+},
+{
+    user : "Scott Estrada",
+    role : "Developer",
+    image : src = "img/scott-estrada-developer.jpg",
+},
+{
+    user : "Barbare Ramos",
+    role : "Graphic Designer",
+    image : src = "img/barbara-ramos-graphic-designer.jpg",
+},
+];
